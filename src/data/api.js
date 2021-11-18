cf760bc19d0928b4d2ad8b948ff60239282d6354
Uzair Ali <uzairali19b@gmail.com>
2021-11-18 20:48:16 +0500

    update POST method

--- src/data/api.js
+++ src/data/api.js
@@ -1,16 +1,27 @@
-const url =
-  'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games';
-const api = 'auuNVZySyDUue602v5rq';
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games';
+const api = 'gQTes67aIhMF5vgksRcG';
 const link = `${url}/${api}/scores`;
 
 const getData = async () => {
   const scores = await fetch(link)
     .then((res) => res.json())
     .then((resData) => resData.result)
-    .catch((err) => {
-      return err;
-    });
+    .catch((err) => err);
   return scores;
 };
 
-export { getData };
+const postData = async (userName, userScore) => {
+  const dataPost = await fetch(link, {
+    method: 'POST',
+    body: JSON.stringify({
+      user: userName.value,
+      score: userScore.value,
+    }),
+    headers: {
+      'Content-type': 'application/json',
+    },
+  });
+  return dataPost;
+};
+
+export { getData, postData };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
 import 'bulma/css/bulma.css';
 import './style.css';
-import { getData } from './data/api';
+import { getData, postData } from './data/api';
 
 const table = document.querySelector('#data-body');
+const name = document.querySelector('#name');
+const score = document.querySelector('#score');
+const form = document.querySelector('#submit');
+const refresh = document.querySelector('#refresh');
+
+form.addEventListener('click', (e) => {
+  e.preventDefault();
+  postData(name, score);
+  name.value = '';
+  score.value = '';
+});
+
+refresh.addEventListener('click', (e) => {
+  e.preventDefault();
+  getData().then((v) => {
+    if (table.children.length !== v.length) {
+      table.innerHTML = '';
+      v.forEach((v) => {
+        const dataBody = `
+              <th>${v.user}</th>
+              <td>${v.score}</td>`;
+        const tableRow = document.createElement('tr');
+        tableRow.innerHTML = dataBody;
+        table.appendChild(tableRow);
+      });
+    }
+  });
+});
 
 getData().then((v) => {
   v.forEach((v) => {
